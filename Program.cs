22d642836efe93d8cfee97a10b890d3bcb0898d9
Stiven1305 <54494817+Stiven1305@users.noreply.github.com>
2019-09-22 19:52:47 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace QUIZ09092019
+{
+    0 references
+    class Program
+    {
+        0 references
+        static void Main(string[] args)
+        {
+            BangunDatar obj=new BangunDatar();
+
+            obj.LuasPersegi();
+
+            obj.LuasSegitiga();
+
+            obj.LuasLingkaran();
+        }
+    }
+}
